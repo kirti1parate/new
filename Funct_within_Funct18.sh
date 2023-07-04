1f6ee0af8dcbdc39758472ab0f2ff9edf18e5013
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-07-04 19:33:55 +0530

    Funct_within_Funct18.sh

---
+++ Funct_within_Funct18.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+number_one(){
+
+echo "This is first function speaking...."
+number_two
+}
+
+number_two(){
+      echo "This is now the second function speaking"
+    }
+#calling function one
+number_one
